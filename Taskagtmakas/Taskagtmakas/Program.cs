23569bed0d5541e7959ab2441284f5fce69e44b8
semihatesci <95163795+semihatesci@users.noreply.github.com>
2022-12-29 18:24:52 +0300

    Add files via upload

---
+++ Taskagtmakas/Taskagtmakas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Taskagtmakas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaç kişi oynanacağını giriniz");
+            int g1 = Convert.ToInt32(Console.ReadLine());
+            Game g = new Game(g1);
+            g.oyna();
+            while (true) { g.kazan(); }
+           
+            
+            
+           
+         
+        }
+    }
+}
